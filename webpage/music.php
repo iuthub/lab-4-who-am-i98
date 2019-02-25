2bfbb5f212c6fa06ed58f6cc71625c1a41aca46f
Jasur 98 <jasurbekyoldoshev98@gmail.com>
2019-02-25 11:13:32 +0500

    readme info added

--- webpage/music.php
+++ webpage/music.php
@@ -2,6 +2,12 @@
 
 	$folder = "songs/";
 	$files = scandir($folder);
+	
+	if(isset($_REQUEST['playlist'])){
+		$playlist = $_REQUEST['playlist'];
+		$musics = (file_get_contents($folder.$playlist));
+		$files = explode("\n",$musics);
+	}
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
@@ -16,28 +22,40 @@
 		<div id="header">
 
 			<h1>190M Music Playlist Viewer</h1>
-			<h2>Search Through Your Playlists and Music</h2>
+			<h2>Search Through Your Playlists and Music <?php echo pathinfo($files[0])['extension']; ?></h2>
 		</div>
 
 
 		<div id="listarea">
 			<ul id="musiclist">
-				<?php foreach($files as $file){ ?>
-					<?php if(pathinfo($file)['extension'] == 'mp3'){ ?>
+				<?php for($i=0;$i<count($files);$i++){ ?>
+					<?php if(pathinfo($files[$i])['extension'] == 'mp3'){ ?>
 						<li class="mp3item">
-							<a href="<?php echo $folder.$file; ?>" download><?php echo basename($file); ?></a>
-							(<?php echo filesize($folder.$file); ?> b)
+							<a href="<?php echo $folder.$files[$i]; ?>" download><?php echo basename($files[$i]); ?></a>
+							(
+								<?php 
+									$size = filesize($folder.$files[$i]);
+									if($size < 1023 ){
+										echo $size." b";
+									}else if($size>1024 && $size<1048575){
+										echo round($size/10**3,2)." kb";
+									}else if($size > 1048576){
+										echo round($size/10**6,2)." mb";
+									}
+								?> 
+							)
 						</li>
 					<?php } ?>
 				<?php } ?>
 
-				<li class="playlistitem">
-					<a href="music.php?playlist=mypicks.txt">mypicks.txt</a>
-				</li>
+				<?php foreach($files as $file){ ?>
+					<?php if(pathinfo($file)['extension'] == 'txt'){ ?>
+						<li class="playlistitem">
+							<a href="music.php?playlist=<?php echo basename($file); ?>"><?php echo basename($file); ?></a>
+						</li>
+					<?php } ?>
+				<?php } ?>
 
-				<li class="playlistitem">
-					<a href="music.php?playlist=playlist.txt">playlist.txt</a>
-				</li>
 			</ul>
 		</div>
 	</body>
